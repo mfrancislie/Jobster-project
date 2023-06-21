d34873dc84db54fa542540db6294b5df26e84ffd
Francislie <mfrancislie@gmail.com>
2023-06-20 22:33:30 -0700

    added pages components

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import DashBoardPage from './DashBoardPage';
+import ErrorPage from './ErrorPage';
+import RegisterPage from './RegisterPage';
+import LandingPage from './LandingPage';
+
+export { DashBoardPage, ErrorPage, RegisterPage, LandingPage };
