5dc479f778adc68c849711297c22b3886206bf26
Francislie <mfrancislie@gmail.com>
2023-07-09 23:21:23 -0700

    userSlice clear value

--- src/pages/dashboard/AddJobPage.js
+++ src/pages/dashboard/AddJobPage.js
@@ -3,7 +3,7 @@ import { FormRow, FormRowSelect } from '../../components';
 import Wrapper from '../../assets/wrappers/DashboardFormPage';
 import { useSelector, useDispatch } from 'react-redux';
 import { toast } from 'react-toastify';
-import { handleChange } from '../../features/job/jobSlice';
+import { clearValues, handleChange } from '../../features/job/jobSlice';
 
 const AddJobPage = () => {
   const {
@@ -84,7 +84,7 @@ const AddJobPage = () => {
             <button
               type="button"
               className="btn btn-block clear-btn"
-              onClick={() => console.log('clear values')}
+              onClick={() => dispatch(clearValues())}
             >
               clear
             </button>
