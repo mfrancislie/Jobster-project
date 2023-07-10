6a80b8b78064e4f7231e89d580c1cb3a814fedb2
Francislie <mfrancislie@gmail.com>
2023-07-09 21:40:44 -0700

    refactoring async functionality

--- README.md
+++ README.md
@@ -1522,3 +1522,79 @@ logoutUser: (state) => {
       removeUserFromLocalStorage();
     },
 ```
+
+#### 49) Refactor Async Functionality
+
+- features/user/userThunk.js
+
+```js
+import customFetch from '../../utils/axios';
+
+import { logoutUser } from './userSlice';
+
+export const registerUserThunk = async (url, user, thunkAPI) => {
+  try {
+    const resp = await customFetch.post(url, user);
+    return resp.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data.msg);
+  }
+};
+
+export const loginUserThunk = async (url, user, thunkAPI) => {
+  try {
+    const resp = await customFetch.post(url, user);
+    return resp.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data.msg);
+  }
+};
+
+export const updateUserThunk = async (url, user, thunkAPI) => {
+  try {
+    const resp = await customFetch.patch(url, user, {
+      headers: {
+        authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+      },
+    });
+    return resp.data;
+  } catch (error) {
+    // console.log(error.response);
+    if (error.response.status === 401) {
+      thunkAPI.dispatch(logoutUser());
+      return thunkAPI.rejectWithValue('Unauthorized! Logging Out...');
+    }
+    return thunkAPI.rejectWithValue(error.response.data.msg);
+  }
+};
+```
+
+userSlice.js
+
+```js
+import {
+  loginUserThunk,
+  registerUserThunk,
+  updateUserThunk,
+} from './userThunk';
+
+export const registerUser = createAsyncThunk(
+  'user/registerUser',
+  async (user, thunkAPI) => {
+    return registerUserThunk('/auth/register', user, thunkAPI);
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  'user/loginUser',
+  async (user, thunkAPI) => {
+    return loginUserThunk('/auth/login', user, thunkAPI);
+  }
+);
+export const updateUser = createAsyncThunk(
+  'user/updateUser',
+  async (user, thunkAPI) => {
+    return updateUserThunk('/auth/updateUser', user, thunkAPI);
+  }
+);
+```
--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,11 +1,15 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
-import customFetch from '../../utils/axios';
 import {
   addUserToLocalStorage,
   getUserFromLocalStorage,
   removeUserFromLocalStorage,
 } from '../../utils/localStorage';
+import {
+  loginUserThunk,
+  registerUserThunk,
+  updateUserThunk,
+} from './userThunk';
 
 const initialState = {
   isLoading: false,
@@ -17,24 +21,14 @@ export const registerUser = createAsyncThunk(
   'user/registerUser',
 
   async (user, thunkAPI) => {
-    try {
-      const resp = await customFetch.post('/auth/register', user);
-      return resp.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data.msg);
-    }
+    return registerUserThunk('auth/register', user, thunkAPI);
   }
 );
 
 export const loginUser = createAsyncThunk(
   'user/loginUser',
   async (user, thunkAPI) => {
-    try {
-      const resp = await customFetch.post('/auth/login', user);
-      return resp.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue(error.response.data.msg);
-    }
+    return loginUserThunk('/auth/loginUser', user, thunkAPI);
   }
 );
 
@@ -42,22 +36,7 @@ export const loginUser = createAsyncThunk(
 export const updateUser = createAsyncThunk(
   'user/updateUser',
   async (user, thunkAPI) => {
-    try {
-      const resp = await customFetch.patch('/auth/updateUser', user, {
-        headers: {
-          // authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
-          authorizaiton: `Bearer`,
-        },
-      });
-      return resp.data;
-    } catch (error) {
-      // console.log(error.response);
-      if (error.response.status === 401) {
-        thunkAPI.dispatch(logoutUser());
-        return thunkAPI.rejectWithValue('Unauthorized! Logging Out...');
-      }
-      return thunkAPI.rejectWithValue(error.response.data.msg);
-    }
+    return updateUserThunk('/auth/updateUser', user, thunkAPI);
   }
 );
 
