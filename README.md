f303628b02324bc415db6578c574b11ea5e68df4
Francislie <mfrancislie@gmail.com>
2023-07-17 20:18:07 -0700

    authHeader axios interceptor approach

--- README.md
+++ README.md
@@ -2612,3 +2612,36 @@ jobThunk.js
 ```js
 import authHeader from '../../utils/authHeader';
 ````
+
+#### 72) AuthHeader - Axios Interceptors Approach
+
+- utils/axios.js
+
+````js
+import axios from 'axios';
+import { getUserFromLocalStorage } from './localStorage';
+
+const customFetch = axios.create({
+  baseURL: 'https://jobify-prod.herokuapp.com/api/v1/toolkit',
+});
+
+customFetch.interceptors.request.use(
+  (config) => {
+    const user = getUserFromLocalStorage();
+    if (user) {
+      config.headers['Authorization'] = `Bearer ${user.token}`;
+      // in the latest version "common" returns undefined
+      // config.headers.common['Authorization'] = `Bearer ${user.token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default customFetch;
+```
+
+- remove auth header
+````
--- src/features/job/JobThunk.js
+++ src/features/job/JobThunk.js
@@ -1,11 +1,10 @@
-import authHeader from '../../utils/authHeader';
 import customFetch from '../../utils/axios';
 import { showLoading, hideLoading, getAllJobs } from '../allJobs/allJobsSlice';
 import { clearValues } from './jobSlice';
 
 export const createJobThunk = async (job, thunkAPI) => {
   try {
-    const resp = await customFetch.post('/jobs', job, authHeader(thunkAPI));
+    const resp = await customFetch.post('/jobs', job);
     thunkAPI.dispatch(clearValues());
     return resp.data;
   } catch (error) {
@@ -16,10 +15,7 @@ export const createJobThunk = async (job, thunkAPI) => {
 export const deleteJobThunk = async (jobId, thunkAPI) => {
   thunkAPI.dispatch(showLoading());
   try {
-    const resp = await customFetch.delete(
-      `/jobs/${jobId}`,
-      authHeader(thunkAPI)
-    );
+    const resp = await customFetch.delete(`/jobs/${jobId}`);
     thunkAPI.dispatch(getAllJobs());
     return resp.data;
   } catch (error) {
@@ -30,11 +26,7 @@ export const deleteJobThunk = async (jobId, thunkAPI) => {
 
 export const editJobThunk = async ({ jobId, job }, thunkAPI) => {
   try {
-    const resp = await customFetch.patch(
-      `/jobs/${jobId}`,
-      job,
-      authHeader(thunkAPI)
-    );
+    const resp = await customFetch.patch(`/jobs/${jobId}`, job);
     thunkAPI.dispatch(clearValues());
     return resp.data;
   } catch (error) {
