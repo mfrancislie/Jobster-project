3ca9d8575252f7c7a8513bcc8d6a32f404a3910e
Francislie <mfrancislie@gmail.com>
2023-07-21 06:09:51 -0700

    UnAuthorized Request

--- src/features/allJobs/allJobsThunk.js
+++ src/features/allJobs/allJobsThunk.js
@@ -1,4 +1,4 @@
-import customFetch from '../../utils/axios';
+import customFetch, { checkForUnauthorizedResponse } from '../../utils/axios';
 
 export const getAllJobsThunk = async (_, thunkAPI) => {
   const { page, search, searchStatus, searchType, sort } =
@@ -23,6 +23,6 @@ export const showStatsThunk = async (_, thunkAPI) => {
     const resp = await customFetch.get('/jobs/stats');
     return resp.data;
   } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data.msg);
+    return checkForUnauthorizedResponse(error, thunkAPI);
   }
 };
